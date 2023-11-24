9c9f0adf84b000c56e66ca8133c3034fad5857f2
Zion Pagad <133878473+Zanis3@users.noreply.github.com>
2023-11-24 21:28:47 +0800

    Add files via upload

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,9 @@
 window.onload = function(){
-    const menu_btn = document.querySelector('#hamburger-menu');
+    const menu_btn = document.querySelector('.hamburger-menu');
+    const nav_mobile = document.querySelector('.mobile-nav');
 
     menu_btn.addEventListener('click', function() {
         menu_btn.classList.toggle('is-active');
+        nav_mobile.classList.toggle('is-active');
     })
 }
