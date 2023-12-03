59b1931dbfbafdeeea7f161a43f30b8c4e1c6e26
Zion Pagad <133878473+Zanis3@users.noreply.github.com>
2023-12-03 23:46:44 +0800

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+const closePopup = document.querySelectorAll('[data-close-button]');
+const overlayPopup = document.getElementById('overlay');
+
+closePopup.forEach(button => {
+    button.addEventListener('click', () => {
+        const closePopupButton = button.closest('.advertisement-popup');
+        closeModal(closePopupButton);
+    })
+})
+
+function closeModal(closePopupButton){
+    if(closePopupButton == null)return;
+    closePopupButton.classList.remove('popup-active');
+    overlayPopup.classList.remove('popup-active');
+}
